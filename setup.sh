20067df67163ee947c26009e9d3e68b3648d49d6
Aaron Chang <aaronchang153.gmail.com>
2019-08-09 20:10:28 -0700

    Allow for custom path on windows

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+if [ $# == 0 ]; then
+    VIM="$HOME/.vim"
+    VIMRC="$HOME/.vimrc"
+elif [ $# == 2 ]; then
+    if [ "$1" == "--win" ]; then
+        VIM="$2/vimfiles"
+        VIMRC="$2/_vimrc"
+    else
+        echo "Fatal: Unrecognized argument: $1"
+        exit 1
+    fi
+else
+    echo "Usage: $0 [--win <home-path>]"
+    exit 1
+fi
+
+echo ".vim path:   $VIM"
+echo ".vimrc path: $VIMRC"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+
 declare -a repo_list=("https://github.com/vim-airline/vim-airline.git" \
                       "https://github.com/vim-airline/vim-airline-themes.git" \
                       "https://github.com/dense-analysis/ale.git" \
@@ -8,25 +29,25 @@ declare -a repo_list=("https://github.com/vim-airline/vim-airline.git" \
                       "https://github.com/scrooloose/nerdtree.git" \
                       "https://github.com/justinmk/vim-syntax-extra.git")
 
-if [ -f "$HOME/.vimrc" ]; then
-    echo "Fatal: File ~/.vimrc already exists"
+if [ -f "$VIMRC" ]; then
+    echo "Fatal: File $VIMRC already exists"
     exit 1
 fi
-if [ -d "$HOME/.vim" ]; then
-    echo "Fatal: Directory ~/.vim already exists"
+if [ -d "$VIM" ]; then
+    echo "Fatal: Directory $VIM already exists"
     exit 1
 fi
 
 orig_dir=$(pwd)
 
-cp .vimrc $HOME/.vimrc
-cp -r .vim $HOME/.vim
+cp .vimrc $VIMRC
+cp -r .vim $VIM
 
-if [ ! -d "$HOME/.vim/bundle" ]; then
-    mkdir $HOME/.vim/bundle
+if [ ! -d "$VIM/bundle" ]; then
+    mkdir $VIM/bundle
 fi
 
-cd $HOME/.vim/bundle
+cd $VIM/bundle
 
 for repo in ${repo_list[@]}; do
     git clone $repo
